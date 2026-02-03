e5c7f14bc2a610acec7f383d2c62de571e29eaba
Jenil1105 <unselfstudy@gmail.com>
2026-02-03 18:01:06 +0530

    added nav and footer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const ejs = require('ejs');
 const Listing = require('./models/listing');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 
 
@@ -11,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+
+
 MONGO_URI = 'mongodb://localhost:27017/farhome';
 
 main().then(() => console.log("MongoDB connected,,, yayyy"))
@@ -22,6 +26,7 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
